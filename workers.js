33890629d664c8670e8552ad5e516cdcf716d232
მოιя ∂αяк <16276003+amirhmz@users.noreply.github.com>
2023-04-01 01:50:24 +0330

    Create workers.js

---
+++ workers.js
@@ -0,0 +1,22 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const serverListUrl = 'https://raw.githubusercontent.com/amirhmz/v2ray-vip/main/servers.txt';
+  const serverListResponse = await fetch(serverListUrl);
+  const serverListText = await serverListResponse.text();
+  const serverList = serverListText.split('\n');
+  
+  for (let i = serverList.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [serverList[i], serverList[j]] = [serverList[j], serverList[i]];
+  }
+  
+  const selectedServer = serverList[Math.floor(Math.random() * serverList.length)];
+  
+  const url = new URL(request.url);
+  url.host = selectedServer;
+  
+  return fetch(url, request);
+}
