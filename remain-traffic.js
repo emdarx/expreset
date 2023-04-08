7f9953e748b325ae3487ccede8a3d375e9f7a81c
Amir Dark <16276003+emdarx@users.noreply.github.com>
2023-04-08 14:01:23 +0330

    Update remain-traffic.js

--- remain-traffic.js
+++ remain-traffic.js
@@ -5,22 +5,27 @@ addEventListener('scheduled', event => {
 })
 
 async function handleScheduledEvent() {
-  const uuid='1E2782BB-AC2E-402F-A362-5A1C467D8051';
-  const telegram_token='619362355555555555555555555555555555555555';
-  const telegram_chat_id='00000000';
-  const url = "https://getafreenode.com/vendor/api.php?act=getuserinfo&uuid="+uuid;
-  const response = await fetch(url);
-  const json = await response.json();
-  console.log(json);
-  if (json.hasOwnProperty("remainingTraffic")) {
-    const remaining_traffic = parseFloat(json.remainingTraffic);
-    console.log(remaining_traffic);
-    const message = `🌏 ترافیک باقی مانده: ${remaining_traffic} گیگابایت\n\n@Expreset Bronze سرویس`;
-    const encoded_message = encodeURIComponent(message);
-    const telegram_url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=${telegram_chat_id}&text=${encoded_message}`;
-    const telegram_response = await fetch(telegram_url);
-    console.log(`Telegram API response: ${telegram_response.status}`);
-  } else {
-    console.log("Unable to find remaining traffic information in the response.");
+  const telegram_token = '619300000000000000000000000000000000jYUjPW2DY'; // توکن ربات تلگرام خودتان را وارد کنید
+  const users_url = 'https://raw.githubusercontent.com/emdarx/expreset/main/user/list.txt'; // لینک فایل حاوی uuid ها در گیت‌هاب
+  const users_response = await fetch(users_url); // درخواست برای دریافت فایل لیست کاربران
+  const users_text = await users_response.text(); // متن فایل لیست کاربران
+  const users_list = users_text.split('\n').filter(Boolean); // تبدیل متن فایل به لیست uuid ها
+
+  for (const uuid of users_list) {
+    const [telegram_id, user_uuid] = uuid.split(","); // تقسیم uuid به تلگرام id و uuid کاربر
+
+    const url = `https://getafreenode.com/vendor/api.php?act=getuserinfo&uuid=${user_uuid}`;
+    const response = await fetch(url);
+    const json = await response.json();
+    if (json.hasOwnProperty('remainingTraffic')) {
+      const remaining_traffic = parseFloat(json.remainingTraffic);
+      const message = `🌏 ترافیک باقی مانده: ${remaining_traffic} گیگابایت\n\n@Expreset Bronze سرویس`;
+      const encoded_message = encodeURIComponent(message);
+      const telegram_url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=${telegram_id}&text=${encoded_message}`;
+      const telegram_response = await fetch(telegram_url);
+      console.log(`Telegram API response for UUID ${user_uuid}: ${telegram_response.status}`);
+    } else {
+      console.log(`Unable to find remaining traffic information for UUID ${user_uuid} in the response.`);
+    }
   }
 }
