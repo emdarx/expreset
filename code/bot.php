2789c93ea160fa59d942951026715d1beb0c926d
Amir Dark <16276003+emdarx@users.noreply.github.com>
2023-04-21 16:45:32 +0330

    Update bot.php

--- code/bot.php
+++ code/bot.php
@@ -1,6 +1,6 @@
 <?php
 
-$bot_token = '1010101010101010101010101010101010101010101010101010101';
+$bot_token = '101010101010101010101010101010101010101010101010101010';
 $file_url = "https://raw.githubusercontent.com/emdarx/expreset/main/user/list.txt";
 $update = json_decode(file_get_contents('php://input'), true);
 $message = $update['message'];
@@ -8,24 +8,31 @@ $chat_id = $message['chat']['id'];
 
 if (strpos($message['text'], '/start') === 0) {
     $response = "به ربات Expreset خوش آمدید\nیک گزینه را انتخاب کنید:";
-    $reply_markup = array(
-        "keyboard" => array(
-                array(
-		array("text" => "🔁 تمدید اشتراک"),
-                array("text" => "🛒 خرید اشتراک")
-            ),
-                array(
-                array("text" => "❎ مشاهده وضعیت اشتراک (ها)")
-            )
+	$reply_markup = array(
+    "keyboard" => array(
+        array(
+            array("text" => "🔁 تمدید اشتراک"),
+            array("text" => "🛒 خرید اشتراک")
         ),
-        "resize_keyboard" => true
-    );
+        array(
+            array("text" => "❎ وضعیت اشتراک"),
+			array("text" => "📖 راهنمای اتصال")
+        )
+    ),
+    "resize_keyboard" => true
+);
+
     send_message($chat_id, $response, $reply_markup);
+}
+
+if ($message['text'] == "📖 راهنمای اتصال") {
+    $response = "راهنمای استفاده از سرویس ها:\n\nبا توجه به اختلالات گسترده در اینترنت کشور و استفاده از سیستم فیلترینگ چینی GFW، دیگر نمیتوان از اشتراک های V2RAY مثل سابق استفاده کرد. زیرا پروتکل های VLESS و VMESS به تنهایی تضمین کننده ارتباط شما نیستند. سعی کنید همیشه از آخرین نسخه برنامه ها استفاده کنید.\n\nپس از دریافت اشتراک خود بصورت کد یا QR CODE آن را در برنامه مخصوص گوشی خود وارد نمایید.\n\n🗳 آیفون: Wings X یا V2BOX\nNapsternetv هم برای اینکار مناسب است اما ما آن را پیشنهاد نمیکنیم\nWingsX: t.ly/XXM6\nV2BOX: t.ly/Zlz-\n\n🗳 اندروید: V2rayng یا matsuri\nv2rayng: t.ly/-gsl\nmatsuri: t.ly/JWOR";
+    send_message($chat_id, $response);
 }
 
 if ($message['text'] == "🛒 خرید اشتراک") {
 //  $text = "💮 سفارش اشتراک Expreset\n\nاشتراک 10 گیگابایت\nلینک خرید: exp10gb.ir.page\n\nشناسه عددی خود را از @USERINFOBOT دریافت کنید و در هنگام پرداخت در کادر مربوطه وارد نمایید.";
-	$text = "⛔️ سفارش اشتراک جدید در حال حاضر امکان پذیر نیست، لطفا در ساعات آینده مجددا تلاش نمایید.";
+	$text = "⛔️ سفارش اشتراک جدید امکان پذیر نیست";
     $parameters = array(
         'chat_id' => $message['chat']['id'],
         'text' => $text
@@ -58,22 +65,21 @@ if ($message['text'] == "🔁 تمدید اشتراک") {
 
         $remaining_traffic = $api_data['remainingTraffic'];
         if ($remaining_traffic < 3) {
-            $response = "اشتراک #".$uuid."\nدارای ".$remaining_traffic." گیگابایت حجم، و قابل تمدید می‌باشد.\nپشتیبانی @eXpresetSupport";
+	    	$response = "🚀 تمدید اشتراک:\n\nبرای تمدید حجم اشتراک #".$uuid." به میزان دلخواه، ابتدا از درگاه نسبت به پرداخت هزینه تمدید اقدام کنید. توجه کنید حتما در توضیحات پرداخت کد UUID اشتراک خود را وارد کنید. اشتراک شما پس از پرداخت بصورت خودکار شارژ و مجددا فعال می‌گردد.\n\n🛍 پرداخت آنلاین: RAHPO.IR.PAGE";
         } else {
-            $response = "⚠️ در حال حاضر تنها تمدید اشتراک های زیر 3GB ممکن است.";
+            $response = "⚠️ در حال حاضر تمدید اشتراک بالای 3GB ممکن نیست";
         }
     }
     send_message($chat_id, $response);
 }
-
-if ($message['text'] == "❎ مشاهده وضعیت اشتراک (ها)") {
-    $user_id = $message['from']['id'];
-    $file_content = file_get_contents($file_url);
-    $user_list = explode("\n", $file_content);
+	if ($message['text'] == "❎ وضعیت اشتراک") {
+		$user_id = $message['from']['id'];
+		$file_content = file_get_contents($file_url);
+		$user_list = explode("\n", $file_content);
 
     $user_uuids = array();
     foreach ($user_list as $user_info) {
-        $user_info_arr = explode(",", $user_info);
+			$user_info_arr = explode(",", $user_info);
         if ($user_info_arr[0] == $user_id) {
             $user_uuids[] = $user_info_arr[1];
         }
@@ -84,7 +90,7 @@ if ($message['text'] == "❎ مشاهده وضعیت اشتراک (ها)") {
         send_message($chat_id, $response);
     }
     else {
-        $response = "";
+        $response = "📊 وضعیت اشتراک ها:\n\n";
         foreach ($user_uuids as $user_uuid) {
             $api_url = "https://getafreenode.com/vendor/api.php?act=getuserinfo&uuid=".$user_uuid;
             $api_response = file_get_contents($api_url);
@@ -94,11 +100,11 @@ if ($message['text'] == "❎ مشاهده وضعیت اشتراک (ها)") {
             $uuid = substr($api_data['uuid'], 0, 8);
 
             if ($remaining_traffic == 0.00) {
-                $response .= "Account #".$uuid."\n";
+                $response .= "اشتراک #".$uuid."\n";
                 $response .= "⚠️ حجم اشتراک به پایان رسیده است\n";
             }
             else {
-                $response .= "Account #".$uuid."\n";
+                $response .= "اشتراک #".$uuid."\n";
                 $response .= "حجم باقی مانده: ".number_format($remaining_traffic, 2)." گیگ\n";
                 $response .= "وضعیت: فعال ✅\n\n";
             }
@@ -107,7 +113,7 @@ if ($message['text'] == "❎ مشاهده وضعیت اشتراک (ها)") {
     }
 }
 
-function send_message($chat_id, $message, $reply_markup = null) {
+	function send_message($chat_id, $message, $reply_markup = null) {
     global $bot_token;
     $url = "https://api.telegram.org/bot".$bot_token."/sendMessage?chat_id=".$chat_id."&text=".urlencode($message);
     if ($reply_markup !== null) {
