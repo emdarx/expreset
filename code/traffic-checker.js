bc3752f66bc7cb219e1d08be178711f056eda961
Amir Dark <16276003+emdarx@users.noreply.github.com>
2023-04-25 12:28:27 +0330

    Update traffic-checker.js

--- code/traffic-checker.js
+++ code/traffic-checker.js
@@ -5,26 +5,31 @@ addEventListener('scheduled', event => {
 })
 
 async function handleScheduledEvent() {
-  const telegram_token = '00000000000000000000000000000000000000000000000';
+  const telegram_token = '1010101010101010101010101010101010101010101010101';
   const users_url = 'https://raw.githubusercontent.com/emdarx/expreset/main/user/list.txt';
   const users_response = await fetch(users_url);
   const users_text = await users_response.text();
   const users_list = users_text.split('\n').filter(Boolean);
+  const eligible_users = [];
   for (const uuid of users_list) {
     const [telegram_id, user_uuid] = uuid.split(",");
-    const display_uuid = user_uuid.split('-')[0];
     const url = `https://getafreenode.com/vendor/api.php?act=getuserinfo&uuid=${user_uuid}`;
     const response = await fetch(url);
     const json = await response.json();
     if (json.hasOwnProperty('remainingTraffic')) {
       const remaining_traffic = parseFloat(json.remainingTraffic);
-      const message = `ترافیک باقی مانده ${remaining_traffic} گیگابایت\n#${display_uuid}`;
-      const encoded_message = encodeURIComponent(message);
-      const telegram_url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=${telegram_id}&text=${encoded_message}`;
-      const telegram_response = await fetch(telegram_url);
-      console.log(`Telegram API response for UUID ${user_uuid}: ${telegram_response.status}`);
+      if (remaining_traffic < 3) {
+        eligible_users.push(user_uuid);
+      }
     } else {
       console.log(`Unable to find remaining traffic information for UUID ${user_uuid} in the response.`);
     }
+  }
+  if (eligible_users.length > 0) {
+    const message = `اشتراک های نیازمند تمدید:\n${eligible_users.join('\n')}`;
+    const encoded_message = encodeURIComponent(message);
+    const telegram_url = `https://api.telegram.org/bot${telegram_token}/sendMessage?chat_id=119626024&text=${encoded_message}`;
+    const telegram_response = await fetch(telegram_url);
+    console.log(`Telegram API response: ${telegram_response.status}`);
   }
 }
