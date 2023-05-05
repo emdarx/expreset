15cdbe1230622e082efc14b4494595e4a6f8168c
Amir Dark <16276003+emdarx@users.noreply.github.com>
2023-05-05 10:46:21 +0330

    Delete code.py

--- code.py
+++
@@ -1,1 +0,0 @@
-vless://a4947cd5-b591-4938-939f-e5c1eeef0e6d@ex61235.amirdark.click:443?security=tls&sni=6cfsli9i533.expreset.click&type=ws&path=/vless&host=server15.expreset.click#Expreset%20%2361235
